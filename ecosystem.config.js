d84946d9a765a842185d74f951bd876d0d3cdc6b
domingues93 <glederson.r.o@gmail.com>
2021-07-18 12:36:39 -0300

    atualizado configurações de deploy.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,17 @@ module.exports = {
   apps : [{
 	name: "cadunesc.com.br",
 	script: 'php artisan serve',
-  }]
+  }],
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '51.79.87.90',
+      ref  : 'origin/master',
+      repo : 'git@github.com:domingues93/cadunesc.com.br.git',
+      path : '/home/ubuntu/cadunesc.com.br/web',
+      'pre-deploy-local': '',
+      'post-deploy' : 'composer install && pm2 startOrRestart ecosystem.config.js',
+      'pre-setup': ''
+    }
+  }
 };
